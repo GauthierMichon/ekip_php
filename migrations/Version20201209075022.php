62426f7361c798ea191fe797ba12fe63611d532b
Gauthier <gauthier.michon@ynov.com>
2020-12-09 08:58:02 +0100

    register/login

--- migrations/Version20201209075022.php
+++ migrations/Version20201209075022.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201110084017 extends AbstractMigration
+final class Version20201209075022 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,8 @@ final class Version20201110084017 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE contrat_type (id INT AUTO_INCREMENT NOT NULL, plein TINYINT(1) NOT NULL, partiel TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE contrats (id INT AUTO_INCREMENT NOT NULL, cdi TINYINT(1) NOT NULL, cdd TINYINT(1) NOT NULL, free TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE offres (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, ville VARCHAR(255) NOT NULL, contrat VARCHAR(255) NOT NULL, contrat_type VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, adresse VARCHAR(255) NOT NULL, code_postal INT NOT NULL, date_creation DATE NOT NULL, date_maj DATE, fin_mission DATE, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE offres (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, ville VARCHAR(255) NOT NULL, contrat VARCHAR(255) NOT NULL, contrat_type VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, adresse VARCHAR(255) NOT NULL, code_postal INT NOT NULL, date_creation DATE NOT NULL, date_maj DATE DEFAULT NULL, fin_mission DATE DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -31,5 +32,6 @@ final class Version20201110084017 extends AbstractMigration
         $this->addSql('DROP TABLE contrat_type');
         $this->addSql('DROP TABLE contrats');
         $this->addSql('DROP TABLE offres');
+        $this->addSql('DROP TABLE user');
     }
 }
